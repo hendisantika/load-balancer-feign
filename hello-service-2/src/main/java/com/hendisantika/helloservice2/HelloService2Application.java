126454a68bff7497d2f0c673cc3c2c713a48be33
Hendi Santika <hendisantika@gmail.com>
2018-02-06 19:41:01 +0700

    #5 - Initial Commit - Hello Service 2

---
+++ hello-service-2/src/main/java/com/hendisantika/helloservice2/HelloService2Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.helloservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloService2Application.class, args);
+	}
+}
