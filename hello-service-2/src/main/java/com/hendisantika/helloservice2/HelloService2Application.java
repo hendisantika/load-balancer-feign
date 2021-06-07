248411bd69dc0e2409ad0140e74ec8ebf7679b7a
Hendi Santika <hendisantika@gmail.com>
2021-06-07 09:11:44 +0700

    #48 - Bug Fix Hello Service 2 after Upgrading into Spring Boot 2.5.0 version

--- hello-service-2/src/main/java/com/hendisantika/helloservice2/HelloService2Application.java
+++ hello-service-2/src/main/java/com/hendisantika/helloservice2/HelloService2Application.java
@@ -1,11 +1,14 @@
 package com.hendisantika.helloservice2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @RestController
 public class HelloService2Application {
+    private Logger logger = LogManager.getLogger(HelloService2Application.class);
 
     @Autowired
-    DiscoveryClient client;
+    DiscoveryClient discoveryClient;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String hello() {
-        ServiceInstance localInstance = client.getLocalServiceInstance();
-        return "Hello World: " + localInstance.getServiceId() + ":" + localInstance.getHost() + ":" + localInstance.getPort();
+        discoveryClient.getServices().forEach(id -> {
+            discoveryClient.getInstances(id).forEach(instance -> {
+                logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+            });
+        });
     }
 
 	public static void main(String[] args) {
--- hello-service/src/main/java/com/hendisantika/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/hendisantika/helloservice/HelloServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class HelloServiceApplication {
     @Autowired
     DiscoveryClient discoveryClient;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public void hello() {
         discoveryClient.getServices().forEach(id -> {
             discoveryClient.getInstances(id).forEach(instance -> {
