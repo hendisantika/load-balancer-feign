6b639a90e891afd7adb8ac65aad4b5c49af304eb
hendisantika <hendisantika@yahoo.co.id>
2024-04-21 06:05:41 +0700

    #152 - Add assertLoadBalancer 🫸🌀✏️📗🐧🐳

--- spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/LoadBalancerIntegrationTest.java
+++ spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/LoadBalancerIntegrationTest.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.Response;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 /**
  * Created by IntelliJ IDEA.
@@ -41,4 +50,24 @@ class LoadBalancerIntegrationTest {
 
     @Autowired
     private WireMockServer secondMockBooksService;
+
+    private void assertLoadBalancer(ReactorLoadBalancer<ServiceInstance> loadBalancer, List<String> hosts) {
+        for (String host : hosts) {
+            Mono<Response<ServiceInstance>> source = loadBalancer.choose();
+            StepVerifier.create(source).consumeNextWith(response -> {
+                then(response).isNotNull();
+                then(response.hasServer()).isTrue();
+
+                ServiceInstance instance = response.getServer();
+                then(instance).isNotNull();
+                then(instance.getHost()).as("instance host is incorrect %s", host).isEqualTo(host);
+
+                if (host.contains("secure")) {
+                    then(instance.isSecure()).isTrue();
+                } else {
+                    then(instance.isSecure()).isFalse();
+                }
+            }).verifyComplete();
+        }
+    }
 }
