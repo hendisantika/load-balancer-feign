e2be5440360c3a2c5be9ce1979b07abf2969e615
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 11:07:03 +0700

    #130 - Add spring-cloud-eureka-feign-client-integration-test ✏️📗 :octocat: 🐳⬆

---
+++ spring-cloud-eureka-feign-client-integration-test/src/main/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/SpringCloudEurekaFeignClientIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springcloudeurekafeignclientintegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaFeignClientIntegrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaFeignClientIntegrationTestApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/SpringCloudEurekaFeignClientIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springcloudeurekafeignclientintegrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaFeignClientIntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
