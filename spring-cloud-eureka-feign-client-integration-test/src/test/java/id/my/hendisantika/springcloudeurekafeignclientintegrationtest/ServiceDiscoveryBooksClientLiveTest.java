b8bea1986db6e3468c777de270b0d01e0fba8bc4
hendisantika <hendisantika@yahoo.co.id>
2024-04-21 06:08:54 +0700

    #160 - Add whenGetBooks_thenTheCorrectBooksAreReturned 🫸🌀✏️📗🐧🐳

--- spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/ServiceDiscoveryBooksClientLiveTest.java
+++ spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/ServiceDiscoveryBooksClientLiveTest.java
@@ -3,7 +3,9 @@ package id.my.hendisantika.springcloudeurekafeignclientintegrationtest;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 import id.my.hendisantika.springcloudeurekafeignclientintegrationtest.client.BooksClient;
+import id.my.hendisantika.springcloudeurekafeignclientintegrationtest.model.Book;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,8 +15,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -45,4 +50,18 @@ class ServiceDiscoveryBooksClientLiveTest {
         await().atMost(60, SECONDS).until(() -> eurekaClient.getApplications().size() > 0);
     }
 
+    /**
+     * Please ensure that Eureka is running on port , 8761 for this test to pass.
+     * The EurekaServerApplication.main in spring-cloud-eureka-server project can be
+     * run to make an Eureka Server available.
+     */
+    @Test
+    public void whenGetBooks_thenTheCorrectBooksAreReturned() {
+        List<Book> books = booksClient.getBooks();
+
+        assertEquals(1, books.size());
+        assertEquals(
+                new Book("Hitchhiker's guide to the galaxy", "Douglas Adams"),
+                books.stream().findFirst().get());
+    }
 }
