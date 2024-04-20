ae31410d64e8460f4eb55bcf8c66de876c4c61d5
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 11:18:41 +0700

    #146 - Add mockBooksService ✏️📗 :octocat: 🐳⬆

--- spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/WireMockConfig.java
+++ spring-cloud-eureka-feign-client-integration-test/src/test/java/id/my/hendisantika/springcloudeurekafeignclientintegrationtest/WireMockConfig.java
@@ -1,6 +1,8 @@
 package id.my.hendisantika.springcloudeurekafeignclientintegrationtest;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 /**
@@ -16,4 +18,8 @@ import org.springframework.test.context.ActiveProfiles;
 @TestConfiguration
 @ActiveProfiles("test")
 public class WireMockConfig {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public WireMockServer mockBooksService() {
+        return new WireMockServer(1030);
+    }
 }
